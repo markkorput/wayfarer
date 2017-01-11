dfe05cfb7bb5e567af11bc6a3f81f0898830301f
Mark van de Korput <mark.korput@gmail.com>
2017-01-11 21:34:46 +0100

    use config npm package; server starts and runs against configured remote mongo server

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ const helmet     = require('helmet');
 const bodyParser = require('body-parser');
 const morgan     = require('morgan');
 const bluebird   = require('bluebird');
+const config     = require('config');
 
-const config = require('./config');
 const routes = require('./routes');
 
 const app  = express();
 
 mongoose.Promise = bluebird;
-mongoose.connect(config.mongo.url);
+mongoose.connect(
+    'mongodb://'+config.get('wayfarer.mongodb.usr')
+    +':'+config.get('wayfarer.mongodb.psw')
+    +'@'+config.get('wayfarer.mongodb.url'));
 
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,8 +22,8 @@ app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use('/', routes);
 
-app.listen(config.server.port, () => {
-  console.log(`Magic happens on port ${config.server.port}`);
+app.listen(config.get('wayfarer.server.port'), () => {
+  console.log(`Magic happens on port ${config.get('wayfarer.server.port')}`);
 });
 
 module.exports = app;
