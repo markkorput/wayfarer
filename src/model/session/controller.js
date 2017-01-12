c1c46290ea9be2b211b112d5b8bf75f7e08646c6
Mark van de Korput <mark.korput@gmail.com>
2017-01-12 15:02:52 +0100

    new GET /session/:id/with/pages API route which returns session data with nested page data

--- src/model/session/controller.js
+++ src/model/session/controller.js
@@ -2,7 +2,17 @@ const BaseController = require('../../lib/controller');
 const facade = require('./facade');
 const config = require('config')
 
-// class Controller extends BaseController {
-// }
+class Controller extends BaseController {
+    findByIdWithPages(req, res, next) {
+        // console.log(req)
+        // return res.status(200).json(req.params)
+        return this.facade.Model.findById(req.params.id).populate('pages').exec()
+        .then(doc => {
+            if (!doc) { return res.status(404).end(); }
+            return res.status(200).json(doc);
+        })
+        .catch(err => next(err));
+    }
+}
 
-module.exports = new BaseController(facade);
+module.exports = new Controller(facade);
--- src/model/session/router.js
+++ src/model/session/router.js
@@ -11,4 +11,7 @@ router.route('/:id')
   .get((...args) => controller.findById(...args))
   .delete((...args) => controller.remove(...args));
 
+router.route('/:id/with/pages')
+  .get((...args) => controller.findByIdWithPages(...args));
+
 module.exports = router;
