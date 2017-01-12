41ee223c76a7f3f74db71dd1fae0c93b0e402c46
Mark van de Korput <mark.korput@gmail.com>
2017-01-12 12:03:57 +0100

    Geo data fetched after API Session create, but fails to persist to DB

--- src/model/session/facade.js
+++ src/model/session/facade.js
@@ -9,9 +9,9 @@ class SessionFacade extends BaseFacade {
         // create like normal REST resource
         return super.create(input)
         // after successfull create
-        .then((doc) => {
+        .then((session_doc) => {
             // initiate browsing session
-            var browsing_session = new BrowsingSession(doc.url, {pageOptions: {cacheFormat: 'HTMLComplete'}});
+            var browsing_session = new BrowsingSession(session_doc.url, {pageOptions: {cacheFormat: 'HTMLComplete'}});
             // every time a page is loaded
             browsing_session.on('page', (session, page) => {
                 // console.log('loaded page: ', page)
@@ -20,13 +20,31 @@ class SessionFacade extends BaseFacade {
                 page.getLinkUrls()
                 .then(link_urls => {
                     this.Model.findByIdAndUpdate(
-                        doc._id,
+                        session_doc._id,
                         {$push: {"pages": {
                             url: page.url,
                             cache_file: page.localCacheFile,
                             hrefs: link_urls}}})
-                    .then(doc => {
-                        console.info('recorded page:', doc.url)
+                    .then(page_doc => {
+                        console.info('recorded page:', page_doc.url)
+
+                        // fetch geo data
+                        page.getGeoData()
+                        .then(geoData => {
+                            // save geo data to this page's record inside the session document
+                            this.Model.update(
+                                { _id: session_doc._id, "pages._id": page_doc._id},
+                                { $set: {"pages.$.geo_data": geoData }})
+                            .then(geo_doc => {
+                                console.log('recorded geo data for ', page.url, ' with: ', geoData, ' \n\ndoc: ', geo_doc)
+                            })
+                            .catch(err => {
+                                console.log('failed to save geo data to db: ', err)
+                            })
+                        })
+                        .catch(err => {
+                            console.warn('failed to fetch geo data for page with url '+page.url+', err:\n', err)
+                        })
                     })
                     .catch(err => {
                         console.warn('failed to add page to db record, err: ', err)
--- src/model/session/model.js
+++ src/model/session/model.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
 
+// based on freegeoip.net json format
+const geoDataScheme = new mongoose.Schema({
+    ip:             { type: String }, // ie. "67.205.11.55"
+    country_code:   { type: String }, // ie. "US"
+    country_name:   { type: String }, // ie. "United States"
+    region_code:    { type: String }, // ie. "CA"
+    region_name:    { type: String }, // ie. "California"
+    city:           { type: String }, // ie. "Brea"
+    zip_code:       { type: String }, // ie. "92821"
+    time_zone:      { type: String }, // ie. "America/Los_Angeles"
+    latitude:       { type: Number }, // ie. 33.9269
+    longitude:      { type: Number }, // ie. -117.8612
+    metro_code:     { type: Number }  // ie. 803
+})
+
 const pageSchema = new mongoose.Schema({
   url:   { type: String, required: true },
   cache_file: { type: String },
-  hrefs: [ String ]
+  hrefs: [ String ],
+  geo_data: { type: geoDataScheme }
 });
 
 const schema = new mongoose.Schema({
